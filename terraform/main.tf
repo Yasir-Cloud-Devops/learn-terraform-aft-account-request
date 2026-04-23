2946ea7719c858aff0dfbcddfe50e386881c0298
Yasir-Cloud-Devops <m.yasir.devops@gmail.com>
2026-04-23 16:04:39 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+module "Test-Account" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "roomailnadeem96+test@gmail.com"
+    AccountName               = "Testing-Account"
+    ManagedOrganizationalUnit = "Account-Factory-OU"
+    SSOUserEmail              = "myasir@go-cloud.io"
+    SSOUserFirstName          = "Testing"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
