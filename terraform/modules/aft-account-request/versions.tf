b16817cde905f58288eeeb076dd4385a3c25cb49
Yasir-Cloud-Devops <m.yasir.devops@gmail.com>
2026-04-23 15:17:35 +0500

    Update versions.tf

--- terraform/modules/aft-account-request/versions.tf
+++ terraform/modules/aft-account-request/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "1.10.5"
+  required_version = ">= 1.6.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.67"
+      version = "~> 5.0, < 5.82.0"
     }
   }
 }
